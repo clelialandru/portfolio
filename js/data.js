d223e37bfcb9dab8ddfd56df812f97eb74a1f78e
Clélia Landru <133856002+clelialandru@users.noreply.github.com>
2023-05-20 18:32:30 +0200

    Moitié fini

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const projects = {
+    "portfolio": {
+        "nom": "Portfolio",
+        "desc": "",
+        "lien": "",
+        "img": "",
+        "link_res": "",
+        "liens_ext": "",
+    },
+
+    "luxiemds": {
+        "nom": "Light The Way • Luxiem Dating Sim",
+        "desc": "Au cours de l'été 2022 et jusqu'à fin avril 2023, je me suis portée volontaire au sein de l'équipe <span class=\"bold_color\">Simporiem Studios</span> en tant que programmeuse. Le jeu est un projet anglophone de jeu vidéo créé avec <span class=\"bold_color\">Ren'Py</span>, un moteur de jeu en langage Python. <br>Ce jeu a été pensé et initialement créé pour l'anniversaire de la mise au monde d'un groupe de talents de l'entreprise Nijisanji : Luxiem. <br>Il s'agit d'un jeu fait par des fans pour les fans.",
+        "lien": "<div class = \"grid_emb\"><iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/rXX2gnxzVCg\" frameborder=\"0\" allow=\" autoplay; picture-in-picture\" allowfullscreen></iframe><p>Clip musical promotionnel</p></div>",
+        "img": "<h2 class = \"sous_titre\">Galerie</h2><div class = \"grid_gal\"><img src = \"../img/img_lds/promo_art.webp\" alt = \"Art Promotionnel de la version de démo du jeu\" width = \"100%\"><img src = \"../img/img_lds/screenshot0002.webp\" alt = \"Menu principal du jeu\" width = \"100%\"><img src = \"../img/img_lds/screenshot0119.webp\" alt = \"Screenshot de la galerie du jeu\" width = \"100%\"><img src = \"../img/img_lds/screenshot0159.webp\" alt = \"Exemple de dialogue du jeu\" width = \"100%\"></div>",
+        "link_res": "",
+        "liens_ext": "<h2 class = \"sous_titre\">Liens pratiques</h2><div class = \'grid_li\'><div class = \"grid_link\"><a href = \"https://twitter.com/luxiemdatingsim\" target = \"_blank\"><img src = \"../img/svg/twitter-circle.svg\" width = \"50%\"></a><a href = \"https://www.facebook.com/luxiemdatingsim\" target = \"_blank\"><img src = \"../img/svg/facebook-circle.svg\" width = \"50%\"></a><a href = \"https://www.instagram.com/luxiemdatingsim/\" target = \"_blank\"><img src = \"../img/svg/instagram-circle.svg\" width = \"50%\"></a><a href = \"https://www.tiktok.com/@luxiemdatingsim\" target = \"_blank\"><img src = \"../img/svg/tiktok-circle.svg\" width = \"50%\"></a><a href = \"https://www.youtube.com/@luxiemdatingsim\" target = \"_blank\"><img src = \"../img/svg/youtube-circle.svg\" width = \"50%\"></a></div><a href = \"https://simporiem-studios.itch.io/light-the-way\" class = \"ext_link\" target = \"_blank\">Lien du projet</a></div>",
+    },
+
+    "noria": {
+        "nom": "Les filles osez l'industrie du futur !",
+        "desc": "Lors de mon année de terminale, j'ai pu, avec quelques unes de mes camarades, participer au projet <span class=\"bold_color\">Noria</span> nommé <span class=\"bold_color\">\"Les filles osez l'industrie du futur\"</span>. J'ai participé dans l'atelier de préparation de contenus en réalité virtuelle en collaboration avec l'entreprise <a href=\"https://ergo-briante.com\" class = \"lien_e\" target = \"_blank\">ERGO Briante.</a> J'ai pu y apprendre des bases de <span class=\"bold_color\">modélisation 3D</span> ainsi que des bases de <span class=\"bold_color\">programmation en C++</span>. J'ai également pu me familiariser avec le moteur de jeu Unity dans le but de créer un environnement en 3D.",
+        "lien": "",
+        "img": "<div class= \"sing_img\"><img src = \"../img/noria/ErgoBriante_PCetPT.webp\" width = \"70%\"></div>",
+        "link_res": "",
+        "liens_ext": "<a href = \"https://www.vehiculedufutur.com/2021-11-24-Les-filles-Osez-lindustrie-du-futur.html#:~:text=Ce%20projet%2C%20réalisé%20par%20le,numérique%20et%20de%20l%27industrie.\" class = \"ext_linka\" target = \"_blank\">Lien du projet</a>",
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+let param = new URLSearchParams(location.search);
+let project = param.get('project');
+
+/*Filter*/
 document.querySelectorAll("input").forEach(e => { e.addEventListener("change", filtre) });
 
 function filtre() {
     document.querySelector(".resultat").classList.toggle(this.name);
 }
+document.querySelector("title").innerText = "Projet • " + projects[project]["nom"];
+
+/*Filling the page of the projects with JS*/
+document.querySelector(".title_proj").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + "</h2>";
+document.querySelector(".title_proj2").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • </h2>";
+document.querySelector(".title_proj3").innerHTML = "<h2 class=\"txti\">" + " " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + "</h2>";
+document.querySelector(".title_proj4").innerHTML = "<h2 class=\"txti\">" + " " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + projects[project]["nom"] + " • " + "</h2>";
+
+document.querySelector(".desc_proj").innerHTML = "<p class=\"para_proj\">" + projects[project]["desc"] + "</p>";
+document.querySelector(".link").innerHTML = projects[project]["lien"];
+document.querySelector(".link_res").innerHTML = projects[project]["link_res"];
+document.querySelector(".images").innerHTML = projects[project]["img"];
+document.querySelector(".proj_title>h1").innerHTML = projects[project]["nom"];
+document.querySelector(".liens_ext").innerHTML = projects[project]["liens_ext"];
